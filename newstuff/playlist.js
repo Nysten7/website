3643e62befb5076ac4f4dd7ddc8193fdcbe226e1
Collin Davis <cwd1222@icloud.com>
2022-02-22 08:39:33 -0500

    If applied, this will update the alert function of webvilletunes.html

---
+++ newstuff/playlist.js
@@ -0,0 +1,13 @@
+var button = document.getElementById("addButton");
+window.onload = init;
+function init(){
+var button = document.getElementById("addButton");
+button.onclick = handleButtonClick;
+}
+function handleButtonClick(){
+var textInput = document.getElementById("songTextInput");
+var songName = textInput.value;
+alert("Adding " + songName);
+}
+button.onclick = hamdleButtonClick;
+
